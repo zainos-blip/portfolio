0e825e45a6e6c1d10176836a942fcee2802c1cf6
zainos-blip <zain.rashid2004@gmail.com>
2025-02-16 14:48:36 +0500

    Fix

--- index.py
+++ index.py
@@ -3,11 +3,8 @@
 from flask import Flask, request, render_template, jsonify
 from flask_limiter import Limiter
 from flask_talisman import Talisman
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 
 csp = {
     'default-src': "'self'",
